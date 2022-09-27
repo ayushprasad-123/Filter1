67d3bd89d4792c50ea4216ca04e1aa3345c4ca7d
ayushprasad-123 <75791807+ayushprasad-123@users.noreply.github.com>
2022-09-27 21:30:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function preload(){}
+function setup(){
+    c=createCanvas(424,300);
+    c.center();
+    cam=createCapture(VIDEO);
+    cam.hide();
+}
+function draw(){
+    image(cam,0,0,424,300);
+}
